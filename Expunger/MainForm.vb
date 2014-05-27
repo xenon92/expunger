1efc3aa6680f0ce49d842b560a65c2c9726a3fb5
Shubhang Rathore <shubhang92@gmail.com>
2014-05-27 16:48:46 +0530

    Add comments in code and cleanup
    - Cleanup
    - Comments give a better explanation of the code
    - Increase some spacing in the code

--- Expunger/MainForm.vb
+++ Expunger/MainForm.vb
@@ -1,33 +1,36 @@
 ﻿Public Class MainForm
 
 
-    Public location1 As String
+
     Private Sub buttonExitClick(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles buttonExit.Click
+
         Application.Exit()
+
     End Sub
-
-    'Private Sub Button3_Click(ByVal sender As System.Object, ByVal e As System.EventArgs)
-    ' FolderBrowserDialog1.ShowDialog()
-
-    ' TextBox1.Text = FolderBrowserDialog1.SelectedPath
-    '  End Sub
 
     Private Sub buttonDeleteClick(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles buttonDelete.Click
 
-        'If TextBox1.Text = "" Or ComboBox1.Text = "" Then
+
         If comboboxExtensions.Text = "" Then
-            MsgBox("Please Choose the Required Details!", MsgBoxStyle.Exclamation)
+
+            ' If the user hasn't selected/written any extension
+            ' in the combobox, throw a MsgBox notifying him to enter an extension
+            ' for the files to be deleted.
+            MsgBox("Enter a file extension to continue...", MsgBoxStyle.Exclamation)
         Else
             Try
+
+                ' Get file extension from the extensions combobox
                 Dim fileExtension As String
                 fileExtension = comboboxExtensions.Text
-                'location1 = TextBox1.Text & "\" & "*." & ext
 
-                'MsgBox(location1)
-
+                ' Merge the extension from the combobox with the
+                ' shell command and execute the whole command in the
+                ' folder where the Expunger.exe lies.
+                ' The command to delete all files of a particular extension is
+                ' del *.extension
                 Dim expungeCommand As String
                 expungeCommand = "del *." & fileExtension
-                'MsgBox(sYourCommand)
                 Shell("cmd /c " & expungeCommand, vbHide)
 
             Catch ex As Exception
@@ -39,15 +42,20 @@
     End Sub
 
     Private Sub ComboBox1_LostFocus(ByVal sender As Object, ByVal e As System.EventArgs) Handles comboboxExtensions.LostFocus
+
+
         If comboboxExtensions.Text = "" Then
             Label4.Text = "extension"
         Else
             Label4.Text = comboboxExtensions.Text
         End If
+
 
     End Sub
 
     Private Sub Form1_MouseClick(ByVal sender As Object, ByVal e As System.Windows.Forms.MouseEventArgs) Handles Me.MouseClick
+
+
         If comboboxExtensions.Text = "" Then
             Label4.Text = "extension"
         Else
@@ -56,4 +64,5 @@
 
 
     End Sub
+
 End Class
